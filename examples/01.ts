56576681f0d0176650c28896cbb7c60568303301
yujingwyh <yujingwyh@163.com>
2020-04-23 14:16:55 +0800

    更新

--- examples/01.ts
+++ examples/01.ts
@@ -4,37 +4,21 @@ analytics({
   modulesScheme: [
     {
       name: 'app-a',
-      syncImport: [
-        {name: 'lib-p'},
-      ],
-      asyncImport: [
-        {
+      syncImport: [{name: 'lib-o'},],
+      asyncImport: [{
           name: 'page-h',
-          syncImport: [{
-            name: 'lib-p',
-          }, {
-            name: 'lib-o-hi'
-          }]
-        },
-        {
+          syncImport: [{name: 'lib-p',}, {name: 'lib-q-hi'}]
+        },{
           name: 'page-i',
-          syncImport: [{
-            name: 'lib-p'
-          }, {
-            name: 'lib-o-hi'
-          }]
-        },
-        {
-          name: 'page-j', syncImport: [{
-            name: 'lib-p'
-          }, {
-            name: 'lib-q-big'
-          }]
+          syncImport: [{name: 'lib-p'}, {name: 'lib-q-hi'}]
+        },{
+          name: 'page-j',
+          syncImport: [{name: 'lib-p'}]
         }
       ]
-    },
-    {
-      name: 'app-b'
+    },{
+      name: 'app-b',
+      syncImport: [{name: 'lib-p'},]
     }
   ]
 });
